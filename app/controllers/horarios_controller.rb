84779766bb03819ba3019656d2a1da88eb42b563
Danilo Lima Dutra <danilolimadutra@gmail.com>
2017-11-20 23:52:17 -0300

    implementado função historico_mensal

--- app/controllers/horarios_controller.rb
+++ app/controllers/horarios_controller.rb
@@ -6,7 +6,7 @@ class HorariosController < ApplicationController
   before_action :require_funcionario, only: [:new, :meus_horarios]
   before_action :require_same_user, only: [:show, :edit, :update, :destroy]
   before_action :require_admin, only: [:index]
-  
+
   # GET /horarios
   # GET /horarios.json
   def index
@@ -24,9 +24,9 @@ class HorariosController < ApplicationController
   end
 
   # GET /horarios/1/edit
-  def edit      
+  def edit
     #impedir edição de horário validado
-    if @horario.hora_valida 
+    if @horario.hora_valida
       flash[:danger] = "Você não pode editar um horário validado."
       redirect_to meus_horarios_horarios_path
     end
@@ -37,15 +37,15 @@ class HorariosController < ApplicationController
   def create
     @horario = Horario.new(horario_params)
     @horario.user = current_user
-    
+
     set_funcionario
     @horario.funcionario_id = @funcionario.id
-    
+
     @horario.total_horas = total_horas
-    
+
     respond_to do |format|
       if @horario.save
-        format.html { 
+        format.html {
           flash[:success] = 'Horário cadastrado com sucesso.'
           redirect_to meus_horarios_horarios_path
         }
@@ -61,10 +61,10 @@ class HorariosController < ApplicationController
   def update
     params_update = horario_params
     params_update[:total_horas] = total_horas
-  
+
     respond_to do |format|
       if @horario.update(params_update)
-        format.html { 
+        format.html {
           flash[:success] = 'Horário atualizado com sucesso.'
           redirect_to meus_horarios_horarios_path
         }
@@ -75,23 +75,23 @@ class HorariosController < ApplicationController
       end
     end
   end
-  
+
   # DELETE /horarios/1
   # DELETE /horarios/1.json
   def destroy
     @horario.destroy
     respond_to do |format|
-      format.html { 
+      format.html {
         flash[:danger] = 'Horários excluído com sucesso'
         redirect_to meus_horarios_horarios_path
       }
       format.json { head :no_content }
     end
   end
-  
+
   def validar_horario
     @horario.hora_valida = true
-    
+
     horario = Hash.new( "horario" )
     horario = {"id" => params[:id]}
 
@@ -104,25 +104,26 @@ class HorariosController < ApplicationController
         redirect_to horarios_path
       end
   end
-  
+
   def validar
     @horarios = Horario.paginate(page: params[:page], per_page: 20)
   end
-  
+
   def meus_horarios
     set_funcionario
     @horarios = @funcionario.horarios.paginate(page: params[:page], per_page: 20).order('data DESC')
-    
+
     @total_extra = @funcionario.horarios.where(:hora_extra => true).sum('total_horas')
-    
+
     @teste = @funcionario.horarios.where(:hora_extra => true).sum('total_horas')
-    
+
     @total_compensado = @funcionario.horarios.where(:hora_extra => false).sum('total_horas')
-    
+
     @saldo_hora = @total_extra - @total_compensado
-    
-    #historico_mensal
-    
+
+    @historico = historico_mensal
+
+    #TODO implementar a exibição do histórico de datas
   end
 
 
@@ -133,45 +134,52 @@ class HorariosController < ApplicationController
       #@horario.inicio = "00:01"
 
     end
-    
+
     def set_funcionario
-        @funcionario = Funcionario.find_by(user_id: current_user.id) 
+        @funcionario = Funcionario.find_by(user_id: current_user.id)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def horario_params
       params.require(:horario).permit(:data, :inicio, :fim, :motivo, :hora_extra)
     end
-    
+
     def total_horas
       hora_inicio = Time.parse(params[:horario][:inicio])
       hora_fim = Time.parse(params[:horario][:fim])
       total_horas = ( ( hora_fim - hora_inicio ) / 60 ) / 60
     end
-    
+
     def require_same_user
       if @horario.user != current_user and !current_user.admin?
         flash[:danger] = "Somente o dono tem permissão."
         redirect_to root_path
       end
     end
-    
-    def historico_mensal
-      date = Time.current - 6.month
-      @horarios = @funcionario.horarios.where("data >= :start_date", start_date: date)
-      
-      i = 0
-      @totais_mensais = Hash.new
-      
-      while i < 7 do
-        extra = 1
-        compensado = 2
-        saldo = 3
-        
-        @totais_mensais[date.strftime("%F")] = [extra, compensado, saldo]
 
-        date += 1.month 
+    def historico_mensal
+      date = Time.now
+      ano = date.strftime("%Y")
+      mes = date.strftime("%m")
+      data_inicio = Date.new(ano.to_i, mes.to_i, 1)
+
+      i = 0
+      lista_datas = {}
+      totais_mensal = {}
+
+      while i < 6 do
+        data_fim = data_inicio + 1.month - 1.day
+
+        extra = @funcionario.horarios.where("hora_extra = ? and data between ? and ?", true, data_inicio, data_fim).sum("total_horas")
+        compensado = @funcionario.horarios.where("hora_extra = ? and data between ? and ?", false, data_inicio, data_fim).sum("total_horas")
+        saldo = extra - compensado
+
+        lista_datas[i] = [data_inicio, extra, compensado, saldo]
+
+        data_inicio = data_inicio - 1.month
         i += 1
       end
+
+      return lista_datas
     end
 end
