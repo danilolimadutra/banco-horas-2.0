e8bb8e2b8982ee51b93a9086d211e298daebc9c1
Danilo Lima Dutra <danilolimadutra@gmail.com>
2018-05-31 23:37:26 -0300

    ajuste no tipo do campo tipo em cadastro de horario

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,51 +17,62 @@
 //= require_tree .
 
 
-function getDataDesvio(){
+function getDataDesvio() {
   var stringData = $('#horario_data').val();
-  var ano = parseInt(stringData.substring(0,4));
-  var mes= parseInt(stringData.substring(5,7))-1;
-  var dia = parseInt(stringData.substring(8,10));
-      
+  var ano = parseInt(stringData.substring(0, 4));
+  var mes = parseInt(stringData.substring(5, 7)) - 1;
+  var dia = parseInt(stringData.substring(8, 10));
+
   return new Date(ano, mes, dia);
 }
 
-function validarForm(){
+function validateTipo() {
+var radios = document.getElementsByName("horario[hora_extra]");
+var formValid = false;
 
-  if ($('#horario_hora_extra').val() == '' ){
-      alert('Obrigatório informar tipo da hora.','i');
-      return false;
-  }
-  
-  if ($('#horario_data').val() == '' ){
-      alert('Obrigatório informar a data.','i');
-      return false;
+  var i = 0;
+  while (!formValid && i < radios.length) {
+    if (radios[i].checked) formValid = true;
+    i++;
   }
 
-  if (getDataDesvio() > new Date()){
-      alert('A data não pode ser futura.','i');
-      return false;
-  }
-  
-  if ($('#horario_inicio').val() == '' ){
-      alert('Obrigatório informar horário de início.','i');
-      return false;
-  }
-  
-  if ($('#horario_fim').val() == '' ){
-      alert('Obrigatório informar horário de fim.','i');
-      return false;
+  if (!formValid) alert('Obrigatório informar tipo da hora.', 'i');
+  return false;
+}
+
+function validarForm() {
+
+  validateTipo();
+
+  if ($('#horario_data').val() == '') {
+    alert('Obrigatório informar a data.', 'i');
+    return false;
   }
 
-  if ($('#horario_fim').val() <= $('#horario_inicio').val()){
-      alert('Horário de fim deve ser maior que horário de início.','i');
-      return false;
+  if (getDataDesvio() > new Date()) {
+    alert('A data não pode ser futura.', 'i');
+    return false;
   }
-  
-  if ($('#horario_motivo').val() == '' ){
-      alert('Obrigatório informar o motivo.','i');
-      return false;
+
+  if ($('#horario_inicio').val() == '') {
+    alert('Obrigatório informar horário de início.', 'i');
+    return false;
   }
-  
+
+  if ($('#horario_fim').val() == '') {
+    alert('Obrigatório informar horário de fim.', 'i');
+    return false;
+  }
+
+  if ($('#horario_fim').val() <= $('#horario_inicio').val()) {
+    alert('Horário de fim deve ser maior que horário de início.', 'i');
+    return false;
+  }
+
+  if ($('#horario_motivo').val() == '') {
+    alert('Obrigatório informar o motivo.', 'i');
+    return false;
+  }
+
   return true;
 }
